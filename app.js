7807eda33cde2c7e721721730721cc813cf832b0
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-11-13 15:40:55 -0300

    :sparkles: feat: criando tarefas

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const criarItem = (texto, status='') => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status}>
+        <div>${texto}</div>
+        <input type="button" value="X">
+    `
+    document.getElementById('todoList').appendChild(item);
+}
